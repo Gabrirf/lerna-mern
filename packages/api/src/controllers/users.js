3fb3b77f368bdfc4d48169a35fab9639bc81790a
Gabriel <gabrirf.dev@gmail.com>
2021-02-11 18:29:23 +0100

    feat(api): Create route to post users

--- packages/api/src/controllers/users.js
+++ packages/api/src/controllers/users.js
@@ -6,7 +6,12 @@ function getUserId(req, res, next) {
   return res.status(200).send(req.params.id);
 }
 
+function createUser(req, res, next) {
+  return res.status(201).send({ message: 'Created' });
+}
+
 module.exports = {
   getUsers,
   getUserId,
+  createUser,
 };
