466c5f25675b5832431b20b219972f1c22832c6c
Gabriel <gabrirf.dev@gmail.com>
2021-02-11 17:24:29 +0100

    feat(front): Add component hook&fetch

--- packages/front/src/app.js
+++ packages/front/src/app.js
@@ -1,10 +1,10 @@
-import React, { Component } from "react";
+import React from "react";
 import { Route, NavLink, BrowserRouter } from "react-router-dom";
-import Home from "./home";
+import Component from "./component";
 import Stuff from "./stuff";
 import Contact from "./contact";
  
-class Main extends Component {
+class Main extends React.Component {
   render() {
     console.log(process.env.PUBLIC_URL);
     return (
@@ -12,12 +12,12 @@ class Main extends Component {
         <div>
           <h1>Simple SPA</h1>
           <ul className="header">
-            <li><NavLink to="/">Home</NavLink></li>
+            <li><NavLink to="/">Component</NavLink></li>
             <li><NavLink to="/stuff">Stuff</NavLink></li>
             <li><NavLink to="/contact">Contact</NavLink></li>
           </ul>
           <div className="content">
-            <Route exact path="/" component={Home}/>
+            <Route exact path="/" component={Component}/>
             <Route path="/stuff" component={Stuff}/>
             <Route path="/contact" component={Contact}/>
           </div>
