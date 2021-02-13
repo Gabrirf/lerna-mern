ca027c7cf6fd5e0b07142e99291748501ad23aab
Gabriel <gabrirf.dev@gmail.com>
2021-02-13 23:17:10 +0100

    feat(front): Change front to Svelte
    Using started template:
    npx @vue/cli create --default front

--- packages/api/src/app.js
+++ packages/api/src/app.js
@@ -6,7 +6,7 @@ const config = require('./config');
 const app = express();
 
 if (process.env.NODE_ENV === 'production') {
-  const buildPath = path.resolve(__dirname, '../../front/build');
+  const buildPath = path.resolve(__dirname, '../../front/dist');
   app.use(express.static(buildPath));
 }
 
